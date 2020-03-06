a9425e118a1aac565532eca5abcf00bab7c7d937
kinta0402 <goro0410@gmail.com>
2020-03-06 05:12:27 +0000

    C6-Finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,31 @@
 class TasksController < ApplicationController
+  before_action :set_user
+
   def index
-    @user = User.find(params[:user_id])
     @tasks = Task.all
   end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+     render :new
+    end
+  end
+  
+  private
+  
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+    
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
