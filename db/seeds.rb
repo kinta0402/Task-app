9e06b6fd323bd35357eb0c6508534b85dcb2cc83
kinta0402 <goro0410@gmail.com>
2020-03-10 09:40:47 +0000

    D7-finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,41 @@ User.create!( name: "管理者",
                password: password,
                password_confirmation: password)
 end
+
+# タスクを自動作成 見本↓
+
+# admin_user = User.first
+# guest_user = User.find(2)
+
+# 50.times do |n|
+#   task_name = "タスク#{n + 1}"
+#   description = "タスク詳細#{n + 1}"
+#   admin_user.tasks.create!(name: task_name, description: description)
+#   guest_user.tasks.create!(name: task_name, description: description)
+# end
+
+# puts "Tasks Created"
+
+50.times do |n|
+  name = "タスク#{n+1}"
+  description = "タスク詳細#{n+1}"
+  Task.create!(name: name,
+               description: description,
+               user_id: "1")
+end
+
+50.times do |n|
+  name = "タスク#{n+1}"
+  description = "タスク詳細#{n+1}"
+  Task.create!(name: name,
+               description: description,
+               user_id: "2")
+end
+
+50.times do |n|
+  name = "タスク#{n+1}"
+  description = "タスク詳細#{n+1}"
+  Task.create!(name: name,
+               description: description,
+               user_id: "3")
+end
